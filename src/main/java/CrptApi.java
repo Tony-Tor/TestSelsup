031fa3f9c87530e52e51d8e1661cbc77d35b04d4
TonyTor <Tony.Tor2016@yandex.ru>
2024-03-28 23:59:02 +0400

    готовое решение

--- src/main/java/CrptApi.java
+++ src/main/java/CrptApi.java
@@ -6,36 +6,25 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CrptApi {
     private final TimeUnit timeUnit;
-    private final Integer requestLimit;
+    private final BlockingQueue<Long> requestTimes;
     private final HttpClient httpClient;
 
     public CrptApi(TimeUnit timeUnit, int requestLimit) {
-        if (requestLimit <= 0) throw new IllegalArgumentException("the request limit must be strictly greater than zero");
-        this.requestLimit = requestLimit;
+        this.requestTimes = new LinkedBlockingQueue<>(requestLimit);
         this.timeUnit = timeUnit;
         this.httpClient = HttpClient.newHttpClient();
     }
 
-    synchronized public void createDocument(String document) throws InterruptedException {
-        /*System.out.println("вход в метод");
-        long currentTime = System.currentTimeMillis(); //получаем текущее время
-        boolean canProceed = false; //проверка возможности исполнения
-        while (!canProceed) { //может выполняться
-            if (requestTimes.remainingCapacity() == 0) { //проверяем есть ли возможность добавить время запроса
-                long oldestRequestTime = requestTimes.take(); //берем элемент сверху и удаляем его, если элементов нет то ждем
-                if (currentTime - oldestRequestTime < timeUnit.toMillis(1)) { //если дельта времени меньше чем единица тайм юнита
-                    System.out.println("wait" + (timeUnit.toMillis(1) - (currentTime - oldestRequestTime)));
-                     - (currentTime - oldestRequestTime)); //усыпить поток на время до окончания секунды
-                }
-            }
-            canProceed = requestTimes.offer(currentTime);
-        }*/
+    public void createDocument(String document) throws InterruptedException {
+        long currentTime = System.currentTimeMillis(); //временная метка запроса
 
-        Thread.sleep(timeUnit.toMillis(1)/requestLimit);
+        boolean canProceed = false;
+        while (!canProceed){
+            canProceed = requestTimes.offer(currentTime);
+        }
 
         try {
             URI uri = URI.create("https://ismp.crpt.ru/api/v3/lk/documents/create");
@@ -50,23 +39,23 @@ public class CrptApi {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println(System.currentTimeMillis()+" end");
+
+        Thread.sleep(timeUnit.toMillis(1));
+
+        requestTimes.remove(currentTime);
     }
 
     public static void main(String[] args) {
         // Пример использования класса CrptApi
         CrptApi crptApi = new CrptApi(TimeUnit.SECONDS, 2);
         String document = "{\"description\": { \"participantInn\": \"string\" }, \"doc_id\": \"string\", \"doc_status\": \"string\", \"doc_type\": \"LP_INTRODUCE_GOODS\", \"importRequest\": true, \"owner_inn\": \"string\", \"participant_inn\": \"string\", \"producer_inn\": \"string\", \"production_date\": \"2020-01-23\", \"production_type\": \"string\", \"products\": [ { \"certificate_document\": \"string\", \"certificate_document_date\": \"2020-01-23\", \"certificate_document_number\": \"string\", \"owner_inn\": \"string\", \"producer_inn\": \"string\", \"production_date\": \"2020-01-23\", \"tnved_code\": \"string\", \"uit_code\": \"string\", \"uitu_code\": \"string\" } ], \"reg_date\": \"2020-01-23\", \"reg_number\": \"string\"}";
-        System.out.println(System.currentTimeMillis()+" start");
-            for (int i = 0; i<=30;i++)
-                new Thread(()->{
-                    System.out.println("запуск потока");
-                    try {
-                        crptApi.createDocument(document);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                        System.out.println("прерывание");
-                    }
-                }).start();
+        for (int i = 0; i<=30;i++)
+            new Thread(()->{
+                try {
+                    crptApi.createDocument(document);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
     }
 }
